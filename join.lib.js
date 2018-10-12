f30fa6802e55605e6b9ea7219bf8fe3658aa5521
jonterad <jonterad@users.noreply.github.com>
2018-10-12 19:52:12 -0400

    adding live alerts

--- join.lib.js
+++ join.lib.js
@@ -246,12 +246,9 @@ JoinForm.prototype.validateForm = function () {
             error_count++;
             hasErrors = true;
             $s(fieldName + "_error").className = "error";
-            // Uncomment the following if statement to add an ARIA alert to the error message
-            // Only the last alert is read, so limit alerts to the first error
-            // so it matches with focus sent to the first message
-            //if(error_count == 1){
-            //	$s(fieldName + "_error").setAttribute("role", "alert");
-            //}
+            if(error_count == 1){
+            	$s(fieldName + "_error").setAttribute("role", "alert");
+            }
             $s(fieldName + "_error").firstChild.nodeValue = error.message;
             if(error_count == 1){
             	$s(fieldName).focus();
@@ -263,9 +260,8 @@ JoinForm.prototype.validateForm = function () {
     if(hasErrors === false){
     	$s("feedback").style.display = "inline-block";
         $s("feedback").firstChild.nodeValue = this.success;
-        $s("feedback").className = "feedback";
-       // Uncomment the next line to add an ARIA alert to the feedback message
-       // $s("feedback").setAttribute("role", "alert");
+        $s("feedback").className = "feedback";       
+       $s("feedback").setAttribute("role", "alert");
     }
     return hasErrors;
 }
